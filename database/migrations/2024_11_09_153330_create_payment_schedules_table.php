2f07db7a789dd30c23a17f116dbad5e7fdca2e1d
swiftly01 <kareemkazeem100@gmail.com>
2024-11-10 07:56:20 +0100

    folder structure

---
+++ database/migrations/2024_11_09_153330_create_payment_schedules_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->references('id')->on('courses')->onDelete('cascade');
+            $table->decimal('amount', 10, 2);
+            $table->string('purpose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_schedules');
+    }
+};
---
+++ database/migrations/2024_11_09_153349_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->decimal('amount', 10, 2)->nullable();
+            $table->string('payment_reference')->nullable();
+            $table->string('status')->nullable();
+            $table->string('purpose');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
