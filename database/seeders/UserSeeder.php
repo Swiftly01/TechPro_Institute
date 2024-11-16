89ced51f5f21548a3b58d8c5715b32a78ce6243c
swiftly01 <kareemkazeem100@gmail.com>
2024-11-16 14:22:32 +0100

    admin email added

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,8 +16,8 @@ class UserSeeder extends Seeder
     {
         DB::table('users')->insert([[
             'name'=>'admin',
-            'email'=>'testadmin@gmail.com',
-            'password'=>Hash::make('1234'),
+            'email'=>'techproinstitutenigeria@gmail.com',
+            'password'=>Hash::make('$techpro+1234'),
          
         ]]);
     }
