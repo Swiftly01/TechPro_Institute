c0d357a7eb5a96da95878b222cfba3d73e20c86a
Swiftly01 <kareemkazeem100@gmail.com>
2025-12-20 06:33:18 +0100

    fix(request): update featured_image size to 3mb

--- app/Http/Requests/UpdateBlogRequest.php
+++ app/Http/Requests/UpdateBlogRequest.php
@@ -28,7 +28,7 @@ class UpdateBlogRequest extends FormRequest
             'excerpt' => ['nullable', 'string', 'max:300'],
             'blog_content' => ['nullable', 'string'],
             'blog_highlight' => ['required', 'string'],
-            'featured_image' => ['nullable', 'image', 'mimes:png,jpg,jpeg', 'max:2048']
+            'featured_image' => ['nullable', 'image', 'mimes:png,jpg,jpeg', 'max:3048']
         ];
     }
 }
